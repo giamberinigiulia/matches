114b6c353d577b2c49f458a059a3faee17d8f982
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-08-20 22:23:39 +0200

    Implemented PlayerController.allPlayers()

--- com.giulia.giamberini.match/src/main/java/com/giulia/giamberini/match/controller/PlayerController.java
+++ com.giulia.giamberini.match/src/main/java/com/giulia/giamberini/match/controller/PlayerController.java
@@ -1,10 +1,20 @@
 package com.giulia.giamberini.match.controller;
 
+import com.giulia.giamberini.match.repository.PlayerRepository;
+import com.giulia.giamberini.match.view.MatchesView;
+
 public class PlayerController {
 
+	private PlayerRepository playerRepository;
+	private MatchesView matchesView;
+
+	public PlayerController(PlayerRepository playerRepository, MatchesView matchesView) {
+		this.playerRepository = playerRepository;
+		this.matchesView = matchesView;
+	}
+
 	public void allPlayers() {
-		// TODO Auto-generated method stub
-		
+		matchesView.showAllPlayers(playerRepository.findAll());
 	}
 
 }
