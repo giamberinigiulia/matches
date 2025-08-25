75e463aaf8e7a7485715281bc12304382beef13b
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-08-25 17:21:15 +0200

    Implemented findAll mapping Document to Match objects

--- com.giulia.giamberini.match/src/main/java/com/giulia/giamberini/match/repository/mongo/MatchRepositoryMongo.java
+++ com.giulia.giamberini.match/src/main/java/com/giulia/giamberini/match/repository/mongo/MatchRepositoryMongo.java
@@ -1,8 +1,10 @@
 package com.giulia.giamberini.match.repository.mongo;
 
 import java.time.LocalDate;
-import java.util.Collections;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.bson.Document;
 
@@ -11,20 +13,24 @@ import com.giulia.giamberini.match.model.Player;
 import com.giulia.giamberini.match.repository.MatchRepository;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 
 public class MatchRepositoryMongo implements MatchRepository {
 
 	private MongoCollection<Document> matchCollection;
 	private MongoCollection<Document> playerCollection;
 
-	public MatchRepositoryMongo(MongoClient client, String dbName, String matchCollectionName, String playerCollectionName) {
+	public MatchRepositoryMongo(MongoClient client, String dbName, String matchCollectionName,
+			String playerCollectionName) {
 		matchCollection = client.getDatabase(dbName).getCollection(matchCollectionName);
 		playerCollection = client.getDatabase(dbName).getCollection(playerCollectionName);
 	}
 
 	@Override
 	public List<Match> findAll() {
-		return Collections.emptyList();
+		return StreamSupport.stream(matchCollection.find().spliterator(), false)
+				.map(this::fromDocumentToMatch)
+				.collect(Collectors.toList());
 	}
 
 	@Override
@@ -38,6 +44,26 @@ public class MatchRepositoryMongo implements MatchRepository {
 
 	@Override
 	public void delete(Player winner, Player loser, LocalDate dateOfTheMatchToDelete) {
+	}
+
+	private Match fromDocumentToMatch(Document d) {
+		String winnerId = d.getString("winnerId");
+		String loserId = d.getString("loserId");
+
+		// find relative winner and loser players
+		Document winnerDoc = playerCollection.find(Filters.eq("_id", winnerId)).first();
+		Document loserDoc = playerCollection.find(Filters.eq("_id", loserId)).first();
+
+		Player winner = fromDocumentToPlayer(winnerDoc);
+		Player loser = fromDocumentToPlayer(loserDoc);
+
+		LocalDate date = d.getDate("date").toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+		return new Match(winner, loser, date);
+	}
+
+	private Player fromDocumentToPlayer(Document d) {
+		return new Player(d.getString("_id"), d.getString("name"), d.getString("surname"));
 	}
 
 }
