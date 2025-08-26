c2c6e7ed230e5ed4e768b6e8fa3c6f1a40c448c3
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-08-26 10:36:02 +0200

    Added failing test for saving a new match in the Repository

--- com.giulia.giamberini.match/src/test/java/com/giulia/giamberini/match/repository/mongo/MatchRepositoryMongoTest.java
+++ com.giulia.giamberini.match/src/test/java/com/giulia/giamberini/match/repository/mongo/MatchRepositoryMongoTest.java
@@ -1,8 +1,12 @@
 package com.giulia.giamberini.match.repository.mongo;
 
+import static com.mongodb.client.model.Filters.eq;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.bson.Document;
 import org.junit.After;
@@ -73,12 +77,12 @@ public class MatchRepositoryMongoTest {
 		matchCollection.insertOne(
 				new Document("winnerId", winner.getId()).append("loserId", loser.getId()).append("date", matchDate));
 	}
-	
+
 	@Test
 	public void testFindByMatchInfoWhenTheMatchDoesntExist() {
 		assertThat(matchRepository.findByMatchInfo(new Player(), new Player(), LocalDate.of(2025, 07, 10))).isNull();
 	}
-	
+
 	@Test
 	public void testFindByMatchInfoWhenTheMatchExistAndIsActuallyFormedAsGiven() {
 		Player winner = new Player("1", "winner name", "winner surname");
@@ -89,8 +93,42 @@ public class MatchRepositoryMongoTest {
 		LocalDate secondMatchDate = LocalDate.of(2025, 07, 15);
 		insertMatchInCollection(winner, loser, firstMatchDate);
 		insertMatchInCollection(winner, loser, secondMatchDate);
-		
-		assertThat(matchRepository.findByMatchInfo(winner, loser, secondMatchDate)).isEqualTo(new Match(winner, loser, secondMatchDate));
+
+		assertThat(matchRepository.findByMatchInfo(winner, loser, secondMatchDate))
+				.isEqualTo(new Match(winner, loser, secondMatchDate));
+	}
+
+	@Test
+	public void testSaveTheMatchInTheDatabase() {
+		Player winner = new Player("1", "winner name", "winner surname");
+		Player loser = new Player("2", "loser name", "loser surname");
+		insertPlayerInCollection(winner);
+		insertPlayerInCollection(loser);
+		LocalDate date = LocalDate.of(2025, 07, 10);
+		Match matchToBeSaved = new Match(winner, loser, date);
+		matchRepository.save(matchToBeSaved);
+		assertThat(StreamSupport.stream(matchCollection.find().spliterator(), false).map(this::fromDocumentToMatch)
+				.collect(Collectors.toList())).containsExactly(matchToBeSaved);
+	}
+
+	private Match fromDocumentToMatch(Document d) {
+		String winnerId = d.getString("winnerId");
+		String loserId = d.getString("loserId");
+
+		// find relative winner and loser players
+		Document winnerDoc = playerCollection.find(eq("_id", winnerId)).first();
+		Document loserDoc = playerCollection.find(eq("_id", loserId)).first();
+
+		Player winner = fromDocumentToPlayer(winnerDoc);
+		Player loser = fromDocumentToPlayer(loserDoc);
+
+		LocalDate date = d.getDate("date").toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+		return new Match(winner, loser, date);
+	}
+
+	private Player fromDocumentToPlayer(Document d) {
+		return new Player(d.getString("_id"), d.getString("name"), d.getString("surname"));
 	}
 
 }
